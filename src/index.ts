74a8c255f7d5dd6edd62e9f50749977efdd7681e
Federico Gimenez <federico.gimenez@gmail.com>
2020-06-12 15:08:51 +0200

    add LabResult types

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+export type Status = 'success' | 'error' | 'in_progress';
+
+export type Value = [number, string];
+
+export type Metric = {
+    name: string;
+}
+
+export interface DataItem {
+    metric: Metric;
+    values: Array<Value>;
+}
+
+export type Data = Array<DataItem>;
+
+export interface LabResult {
+    name: string;
+    startTime: string;
+    endTime: string;
+    status: Status;
+
+    data: Data;
+
+    errorType?: string;
+    error?: string;
+    warnings?: Array<string>;
+}
